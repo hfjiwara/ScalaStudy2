410e1a8e5a59a7809933b8f24a8d92a60fb44571
hfjiwara <hiroto_fujiwara@dwango.co.jp>
2024-11-25 17:40:00 +0900

    for文と内包表記について

--- src/main/scala/ReturnApp.scala
+++ src/main/scala/ReturnApp.scala
@@ -1,15 +1,17 @@
 object ReturnApp {
-  def main(array: Array[String], target: String): Int = {
-    var index = -1
-    var found = false
-    var i = 0
-    while (i < array.length && !found) {
-      if (array(i) == target) {
-        index = i
-        found = true
-      }
+  def main(args: Array[String]): Unit = {
+    val a1: Array[String] = Array("0", "1", "2", "3", "4", "neko")
+    val a2 = "neko"
+    val index = indexOf(a1, a2)
+    println(s"Index of '$a2' :$index")
+  }
+
+  def indexOf(array: Array[String], target: String): Int = {
+   var i = 0
+    while(i < array.length) {
+      if(array(i) == target) return i
       i += 1
     }
-    index
+    -1
   }
 }
