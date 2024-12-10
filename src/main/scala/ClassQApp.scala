6aaeeea55afce87b258857d57eb782d11ba5d5f2
hfjiwara <hiroto_fujiwara@dwango.co.jp>
2024-12-10 14:52:28 +0900

    練習問題解答

--- src/main/scala/ClassQApp.scala
+++ src/main/scala/ClassQApp.scala
@@ -1,19 +1,12 @@
 class ClassQApp(val x: Int, val y: Int, val z: Int) {
-  def +(p: ClassQApp: ClassQApp = {
-    new ClassQApp(x + p.x, y + p.y + z + p.z)
+}
+object ClassQApp {
+  def main(args: Array[String]): Unit = {
+    val p = new ClassQApp(10, 20, 30)
+    println(p.x)
+    println(p.y)
+    println(p.z)
+
   }
 
-  override def toString() = String = "(" + x + ", " + y + ", " + z + ")"
-
 }
-
-object ClassQApp {}
-
-def main(args: Array[String]): Unit = {
-
-  var pre = new ClassQApp()
-
-
-
-}
-
