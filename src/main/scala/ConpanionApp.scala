19f0d3e9cde1c9b65aad830a81b9eb0b60077fed
hfjiwara <hiroto_fujiwara@dwango.co.jp>
2024-12-11 16:11:57 +0900

    add

--- src/main/scala/ConpanionApp.scala
+++ src/main/scala/ConpanionApp.scala
@@ -1,3 +1,20 @@
 class ConpanionApp {
 
+
+}
+
+class neko3(val name:String, val age: Int, val weight:Int) {
+  def getDetails: String= s"nekoname: $name, $age, $weight"
+}
+
+object neko3 {
+  //コンパニオンオブジェクト
+  def applu(name:String, age:Int, weight:Int): Unit = {
+ new neko3(name, age, weight)
+  }
+
+  //デフォルトのメゾっど
+  def defaultMethod: neko3 = {
+    new neko3("nemu". 1. 2)
+  }
 }
